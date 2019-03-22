47846e17b8f8d5c50193b635fe76ad87853d98b7
zhangqi0918 <48822544+zhangqi0918@users.noreply.github.com>
2019-03-22 18:01:07 +0800

    Add files via upload

--- src/main/java/com/expedia/interview/ex2/Exercise2.java
+++ src/main/java/com/expedia/interview/ex2/Exercise2.java
@@ -1,20 +1,30 @@
-package com.expedia.interview.ex2;
-
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+package com.expedia.interview.ex1;
 
 public class Exercise2 {
 
     class SinglyLinkedListNode {
+    	int val;
         SinglyLinkedListNode next;
+        public SinglyLinkedListNode(int val){
+        	this.val=val;
+        }
     }
 
     boolean hasCycle(SinglyLinkedListNode head) {
+    	SinglyLinkedListNode p1=head;
+    	SinglyLinkedListNode p2=head.next;
+    	while(p2!=null&&p2.next!=null){
+    		p1=p1.next;
+    		p2=p2.next.next;
+    		if(p2==null){
+    			return false;
+    		}
+    		int val1=p1.val;
+    		int val2=p2.val;
+    		if(val1==val2){
+    			return true;
+    		}
+    	}
         return false;
     }
 }
