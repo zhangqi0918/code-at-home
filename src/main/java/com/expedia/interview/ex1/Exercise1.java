0e3c87404df94355492ad1214c4b343da8071539
zhangqi0918 <48822544+zhangqi0918@users.noreply.github.com>
2019-03-22 17:04:31 +0800

    Add files via upload

--- src/main/java/com/expedia/interview/ex1/Exercise1.java
+++ src/main/java/com/expedia/interview/ex1/Exercise1.java
@@ -1,16 +1,29 @@
 package com.expedia.interview.ex1;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 public class Exercise1 {
 
-    String timeInWords(int h, int m) {
-        return "";
+    @SuppressWarnings("null")
+	String timeInWords(int h, int m) {
+    	StringBuffer timeBuffer=new StringBuffer();
+    	timeBuffer.append("");
+    	if(h>=1&&h<=12&&m>=0&&m<=60){
+    		if (m==0) {
+    			timeBuffer.append(NumUtil.analyze(h)).append(" o'clock");
+				
+			}else if (m>30) {
+				timeBuffer.append(NumUtil.analyze(60-m)+" minutes to "+NumUtil.analyze(h+1));	
+			}else{
+				if(m==15){
+					timeBuffer.append("quarter "+" past "+NumUtil.analyze(h));
+				}else{
+					timeBuffer.append(NumUtil.analyze(m)+"minutes past "+NumUtil.analyze(h));
+				}		
+			}
+    	}else{
+    		System.out.println("You entered an illegal number");
+    		return " ";
+    	}
+		return timeBuffer.toString();
+    	
     }
 }
